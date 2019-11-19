23c4ff4b9f05b3f6494bf00930b601e16cf7d167
于斯同 <yusitong1999@foxmail.com>
2019-11-19 22:57:39 +0800

    初步完成首页框架

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -17,8 +17,31 @@ Page({
       })
       return
     }
-    
 
+    // 调用云函数 获取openId
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => { // 调用成功
+        // 输出日志
+        console.log('[云函数] [login] user openid: ', res.result.openid)
+        // 全局数据 openid
+        app.globalData.openid = res.result.openid
+        /*
+        wx.navigateTo({ // 跳转
+          url: '../userConsole/userConsole',
+        })
+        */
+      },
+      fail: err => { // 调用失败
+        // 输出错误
+        console.error('[云函数] [login] 调用失败', err)
+        wx.navigateTo({
+          url: '../deployFunctions/deployFunctions',
+        })
+      }
+    }),
+    
 
     // 获取用户信息
     wx.getSetting({
@@ -38,6 +61,10 @@ Page({
     })
   },
 
+  goToSendMessage: function (param) {
+    wx.navigateTo({ url: '../sendMessage/sendMessage', });
+  },
+
   onGetUserInfo: function(e) {
     if (!this.data.logged && e.detail.userInfo) {
       this.setData({
@@ -48,80 +75,6 @@ Page({
     }
   },
 
-  onGetOpenid: function() {
-    // 调用云函数
-    wx.cloud.callFunction({
-      name: 'login',
-      data: {},
-      success: res => { // 调用成功
-        // 输出日志
-        console.log('[云函数] [login] user openid: ', res.result.openid)
-        // 全局数据 openid
-        app.globalData.openid = res.result.openid
-        wx.navigateTo({ // 跳转
-          url: '../userConsole/userConsole',
-        })
-      },
-      fail: err => { // 调用失败
-        // 输出错误
-        console.error('[云函数] [login] 调用失败', err)
-        wx.navigateTo({
-          url: '../deployFunctions/deployFunctions',
-        })
-      }
-    })
-  },
-
-  // 上传图片
-  doUpload: function () {
-    // 选择图片
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['compressed'],
-      sourceType: ['album', 'camera'],
-      success: function (res) {
-        // 加载黑框
-        wx.showLoading({
-          title: '上传中',
-        })
-
-        // 临时文件路径？
-        const filePath = res.tempFilePaths[0]
-        
-        // 上传图片 my-image.{文件扩展名}
-        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
-        wx.cloud.uploadFile({
-          cloudPath,
-          filePath,
-          success: res => {
-            console.log('[上传文件] 成功：', res)
-
-            app.globalData.fileID = res.fileID
-            app.globalData.cloudPath = cloudPath
-            app.globalData.imagePath = filePath
-            
-            wx.navigateTo({
-              url: '../storageConsole/storageConsole'
-            })
-          },
-          fail: e => {
-            console.error('[上传文件] 失败：', e)
-            wx.showToast({
-              icon: 'none',
-              title: '上传失败',
-            })
-          },
-          complete: () => {
-            wx.hideLoading()
-          }
-        })
-
-      },
-      fail: e => {
-        console.error(e)
-      }
-    })
-  },
 
 
 })
